490aa76f1d665806d6cd515b03b4454beeea8063
jbockerstette <jim.bockerstette@gmail.com>
2017-09-06 20:23:47 -0700

    Bootstrap 4 is working.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../src/images/cash-calculator.svg';
 import './App.css';
+import {UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle} from "reactstrap";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +16,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Currency Converter</h2>
         </div>
-
+        <div className="panel panel-default">
+          <h2>Select Currency</h2>
+          <UncontrolledDropdown>
+            <DropdownToggle caret>
+              Dropdown
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </div>
       </div>
     );
   }
