fc4cfeebdfdf27855389b682d798e3bf0f92a6ac
jbockerstette <jim.bockerstette@gmail.com>
2017-09-07 17:08:15 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from '../src/images/cash-calculator.svg';
 import usd_flag from '../src/images/usd.png';
 import aud_flag from '../src/images/aud.png';
 import './App.css';
-import {Card, CardTitle} from "reactstrap";
 import SelectCurrency from "./SelectCurrency";
 import CurrencyConverter from "./CurrencyConverter";
 import {Map} from 'immutable';
@@ -40,7 +39,7 @@ class App extends React.Component {
   }
 
   handleCurrencySelected(currencyName) {
-    const { leftValue } = this.state;
+    const {leftValue} = this.state;
     //TODO: Get the new conversion rate.
     const newConversionRate = 5;
     const newRightValue = leftValue * newConversionRate;
@@ -80,33 +79,31 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <Card className="my-card">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>Currency Converter</h2>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Currency Converter</h2>
+        </div>
+        <div className="row justify-content-center">
+          <h3 className="my-card-title">Select Currency</h3>
+        </div>
+        <SelectCurrency selectedCurrency={selectedCurrency} currencies={currencies}
+                        handleCurrencySelected={this.handleCurrencySelected.bind(this)}/>
+        <div className="row">
+          <div className="col-sm-6 col-md-auto my-col">
+            <CurrencyConverter currency={leftCurrency} value={leftValue}
+                               handleOnChange={this.handleOnChangeLeft.bind(this)}/>
           </div>
-          <div className="row justify-content-center">
-            <CardTitle className="my-card-title">Select Currency</CardTitle>
+          <div className="col-sm-6 col-md-auto my-col">
+            <CurrencyConverter currency={rightCurrency} value={rightValue}
+                               handleOnChange={this.handleOnChangeRight.bind(this)}/>
           </div>
-          <SelectCurrency selectedCurrency={selectedCurrency} currencies={currencies}
-                          handleCurrencySelected={this.handleCurrencySelected.bind(this)}/>
-          <div className="row">
-            <div className="col-6 my-col">
-              <CurrencyConverter currency={leftCurrency} value={leftValue}
-                                 handleOnChange={this.handleOnChangeLeft.bind(this)}/>
-            </div>
-            <div className="col-6 my-col">
-              <CurrencyConverter currency={rightCurrency} value={rightValue}
-                                 handleOnChange={this.handleOnChangeRight.bind(this)}/>
-            </div>
+        </div>
+        <div className="row text-left">
+          <div className="col-12 my-col-exchange-rate">
+            <b>Exchange
+              Rate </b> {`${leftCurrency.symbol} 1 ${leftCurrency.shortName} = ${rightCurrency.symbol} ${conversionRate} ${rightCurrency.shortName}`}
           </div>
-          <div className="row text-left">
-            <div className="col-12 my-col-exchange-rate">
-              <b>Exchange
-                Rate </b> {`${leftCurrency.symbol} 1 ${leftCurrency.shortName} = ${rightCurrency.symbol} ${conversionRate} ${rightCurrency.shortName}`}
-            </div>
-          </div>
-        </Card>
+        </div>
       </div>
     );
   }
